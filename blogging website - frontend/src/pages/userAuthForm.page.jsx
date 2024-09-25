d295dcd192deb480c170cea5804d25039f8d4a49
Shashank <shashankdevulapally569@gmail.com>
2024-09-25 13:00:50 +0530

    modified:   blogging website - frontend/index.html 	modified:   blogging website - frontend/src/components/input.component.jsx 	modified:   blogging website - frontend/src/pages/userAuthForm.page.jsx

--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -1,10 +1,28 @@
+import InputBox from "../components/input.component"
+
 const UserAuthForm = ({type}) => {
     return(
         <section className="h-cover flex items-center justify-center">
             <form className="w-[80%] max-w-[400px]">
-                <h1>
-                    {type}
+                <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
+                    {type=="Sign In" ? "Welcome Back":"Join Us today"}
                 </h1>
+                {
+                    type != "Sign In" ?
+                        <InputBox 
+                            name="fullname"
+                            type="text"
+                            placeholder="Full Name"
+                            icon="fi-rr-user"
+                        />
+                    :""
+                }
+                <InputBox 
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                    icon="fi-sr-envelope"
+                />
             </form>
         </section>
     )
