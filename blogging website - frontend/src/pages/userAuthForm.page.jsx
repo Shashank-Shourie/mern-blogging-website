d3fa590a85425bae81d048445d0e3b36744224f1
Shashank <shashankdevulapally569@gmail.com>
2024-09-26 23:22:52 +0530

    modified:   blogging website - frontend/src/common/page-animation.jsx 	modified:   blogging website - frontend/src/pages/userAuthForm.page.jsx

--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -1,61 +1,64 @@
+import AnimationWrapper from "../common/page-animation"
 import { Link } from "react-router-dom"
 import InputBox from "../components/input.component"
 import googleIcon from "../imgs/google.png"
 
-const UserAuthForm = ({type}) => {
-    return(
-        <section className="h-cover flex items-center justify-center">
-            <form className="w-[80%] max-w-[400px]">
-                <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
-                    {type=="Sign In" ? "Welcome Back":"Join Us today"}
-                </h1>
-                {
-                    type != "Sign In" ?
-                        <InputBox 
-                            name="fullname"
-                            type="text"
-                            placeholder="Full Name"
-                            icon="fi-rr-user"
-                        />
-                    :""
-                }
-                <InputBox 
-                    name="email"
-                    type="email"
-                    placeholder="Email"
-                    icon="fi-sr-envelope"
-                />
-                <InputBox 
-                    name="password"
-                    type="password"
-                    placeholder="Password"
-                    icon="fi-br-lock"
-                />
-                <button className="btn-dark center mt-14" type="submit">
-                    {type}
-                </button>
-                <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
-                    <hr className="w-1/2 border-black"/>
-                    <p>or</p>
-                    <hr className="w-1/2 border-black"/>
-                </div>
-                <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
-                    <img src={googleIcon} className="w-5"/>
-                    continue with google
-                </button>
-                {
-                    type=="Sign In"?
-                        <p className="mt-6 text-dark-grey text-xl text-center">
-                            join us today?
-                            <Link to="/signup" className="underline text-black text-xl ml-1">Sign up here</Link>
-                        </p>:
-                        <p className="mt-6 text-dark-grey text-xl text-center">
-                            Already a member ?
-                            <Link to="/signin" className="underline text-black text-xl ml-1">Sign in here</Link>
-                        </p>
-                }
-            </form>
-        </section>
+const UserAuthForm = ({ type }) => {
+    return (
+        <AnimationWrapper keyValue={type}>
+            <section className="h-cover flex items-center justify-center">
+                <form className="w-[80%] max-w-[400px]">
+                    <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
+                        {type == "Sign In" ? "Welcome Back" : "Join Us today"}
+                    </h1>
+                    {
+                        type != "Sign In" ?
+                            <InputBox
+                                name="fullname"
+                                type="text"
+                                placeholder="Full Name"
+                                icon="fi-rr-user"
+                            />
+                            : ""
+                    }
+                    <InputBox
+                        name="email"
+                        type="email"
+                        placeholder="Email"
+                        icon="fi-sr-envelope"
+                    />
+                    <InputBox
+                        name="password"
+                        type="password"
+                        placeholder="Password"
+                        icon="fi-br-lock"
+                    />
+                    <button className="btn-dark center mt-14" type="submit">
+                        {type}
+                    </button>
+                    <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
+                        <hr className="w-1/2 border-black" />
+                        <p>or</p>
+                        <hr className="w-1/2 border-black" />
+                    </div>
+                    <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
+                        <img src={googleIcon} className="w-5" />
+                        continue with google
+                    </button>
+                    {
+                        type == "Sign In" ?
+                            <p className="mt-6 text-dark-grey text-xl text-center">
+                                join us today?
+                                <Link to="/signup" className="underline text-black text-xl ml-1">Sign up here</Link>
+                            </p> :
+                            <p className="mt-6 text-dark-grey text-xl text-center">
+                                Already a member ?
+                                <Link to="/signin" className="underline text-black text-xl ml-1">Sign in here</Link>
+                            </p>
+                    }
+                </form>
+            </section>
+        </AnimationWrapper>
     )
 }
 
